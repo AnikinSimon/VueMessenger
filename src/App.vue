bdabcc813ef3af470d21966f91eb3f48024db26a
AnikinSimon <s.anikin1309@gmail.com>
2022-12-21 19:02:23 +0300

    Add Container

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyContainer/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyContainer from './components/Container.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyContainer
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
